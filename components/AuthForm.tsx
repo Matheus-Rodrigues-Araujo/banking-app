2d84388d9dba9247a6f97fd0f2fc16db0e64a284
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-05-31 19:00:28 -0300

    fix: NEXT_APPWRITE_KEY value has been updated

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,12 +14,13 @@ import CustomInput from "./CustomInput";
 import { Loader2 } from "lucide-react";
 
 import { authFormSchema } from "@/lib/utils";
-import { signIn, signUp } from "@/lib/actions/user.actions";
+import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  // const loggedInUser = await getLoggedInUser()
 
   const formSchema = authFormSchema(type);
 
@@ -36,9 +37,20 @@ const AuthForm = ({ type }: { type: string }) => {
 
     try {
       if (type === "sign-up") {
-        const newUser = await signUp(data);
-        setUser(newUser)
-        
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+        const newUser = await signUp(userData);
+        setUser(newUser);
       }
       if (type === "sign-in") {
         // const response = await signIn({
